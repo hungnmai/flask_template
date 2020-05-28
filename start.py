82a03dd1c0925723cedcf08c85b618f8696a53ed
Nguyen Hung <hungmanh.hust@gmail.com>
2020-05-28 11:35:21 +0700

    fix api

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from api.routers import app
-
+from utils.get_configs import system_configs
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="8080", debug=True)
+    app.run(host=system_configs['host'], port=system_configs['port'], debug=True)
